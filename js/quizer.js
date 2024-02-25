6964833cd55e74810c98acea57c76c2e3ecbc51c
Koen-varvar <156729428+Koen-varvar@users.noreply.github.com>
2024-02-25 09:04:05 +0300

    Add files via upload

--- js/quizer.js
+++ js/quizer.js
@@ -1564,7 +1564,7 @@ function setup(){
 }
 
 let pack_num;
-let year_url = 'https://sunquiz.netlify.app/2010';
+let year_url = 'https://sunquiz.netlify.app/1990';
 
 function useUrlParam() {
 	var url_string = window.location.href; 
